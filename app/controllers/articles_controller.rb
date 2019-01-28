96acab8bd927a736f656e2b22dec32223f25bb85
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-01-28 15:49:41 +0100

    created crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,45 @@
 class ArticlesController < ApplicationController
   def index
-  end
-  def new
-  end
-  def create
+    @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path(@articles)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path(@articles)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path(@articles)
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
